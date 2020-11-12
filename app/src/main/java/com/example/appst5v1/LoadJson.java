91d6df9af83b4d39eb7bd8989f3cd0066bf23a33
Pierre Mejane <pmdu93@live.fr>
2020-11-12 17:57:20 +0100

    add LoadJSon

---
+++ app/src/main/java/com/example/appst5v1/LoadJson.java
@@ -0,0 +1,64 @@
+package com.example.appst5v1;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import androidx.lifecycle.MutableLiveData;
+
+public class LoadJson {
+
+
+    public static MutableLiveData<String> loadJson(String url){
+            MutableLiveData<String> strResult= new MutableLiveData<>();
+            final AsyncTask<Void, Void, Void> mTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    strResult.setValue(getJsonFromServer(url));
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+
+            }
+
+        };
+
+        mTask.execute();
+        return strResult;
+    }
+
+    public static String getJsonFromServer(String url) throws IOException {
+
+        BufferedReader inputStream = null;
+
+        URL jsonUrl = new URL(url);
+        URLConnection dc = jsonUrl.openConnection();
+
+        dc.setConnectTimeout(5000);
+        dc.setReadTimeout(5000);
+
+        inputStream = new BufferedReader(new InputStreamReader(
+                dc.getInputStream()));
+
+        // read the JSON results into a string
+        String jsonResult = inputStream.readLine();
+        for(int i = 0; i<10;i++){
+            jsonResult+=inputStream.readLine();
+        }
+
+        return jsonResult;
+    }
+}
--- app/src/main/java/com/example/appst5v1/ui/doctor/DoctorFragment.java
+++ app/src/main/java/com/example/appst5v1/ui/doctor/DoctorFragment.java
@@ -1,15 +1,23 @@
 package com.example.appst5v1.ui.doctor;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.appst5v1.MainActivity;
 import com.example.appst5v1.R;
 import com.example.appst5v1.ui.profile.ProfileViewModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -18,12 +26,19 @@ import androidx.lifecycle.ViewModelProvider;
 public class DoctorFragment extends Fragment {
 
     private DoctorViewModel mViewModel;
+    private String str;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mViewModel =
                 new ViewModelProvider(this).get(DoctorViewModel.class);
-        View root = inflater.inflate(R.layout.profile_fragment, container, false);
+
+        View root = inflater.inflate(R.layout.layout_test, container, false);
+        str = "Echec critique";
+        final TextView text = (TextView) root.findViewById(R.id.simple_textview);
+
+
+        /*View root = inflater.inflate(R.layout.profile_fragment, container, false);
 
         ((TextView) root.findViewById(R.id.firstname_profile)).setText("Médecin");
 
@@ -35,7 +50,11 @@ public class DoctorFragment extends Fragment {
                 MainActivity.activityManager.callNumber( ((TextView) root.findViewById(R.id.phone_profile)).getText().toString());
             }
 
-        });
+        });*/
         return root;
 
-    }}
+    }
+
+
+
+}
